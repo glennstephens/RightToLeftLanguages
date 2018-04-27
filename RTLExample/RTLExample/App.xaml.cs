53578c48c9ed7d4764708048fac26c5e110ef511
Glenn Stephens <glenn@orchardebusiness.com>
2018-04-27 22:09:03 +1000

    Initial Commit

---
+++ RTLExample/RTLExample/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Plugin.Multilingual;
+using RTLExample.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace RTLExample
+{
+	public partial class App : Application
+	{
+        public static NavigationPage NavPage = null;
+
+        public App()
+        {
+            InitializeComponent();
+
+            // Set up the initial culture (we're starting this one in English)
+            //CrossMultilingual.Current.CurrentCultureInfo = new CultureInfo("en");
+            //AppResources.Culture = CrossMultilingual.Current.CurrentCultureInfo;
+
+            // To set it up for the device's culture use
+            CrossMultilingual.Current.CurrentCultureInfo = CrossMultilingual.Current.DeviceCultureInfo;
+            AppResources.Culture = CrossMultilingual.Current.DeviceCultureInfo;
+
+            NavPage = new NavigationPage(new SelectLanguagePage());
+            MainPage = NavPage;
+        }
+
+        protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ RTLExample/RTLExample/Pages/SelectLanguagePage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Plugin.Multilingual;
+using RTLExample.Resources;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RTLExample
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SelectLanguagePage : ContentPage
+	{
+		public SelectLanguagePage ()
+		{
+			InitializeComponent ();
+		}
+
+        void ShowLanguage(string language, bool showRightToLeft)
+        {
+            CrossMultilingual.Current.CurrentCultureInfo = new CultureInfo(language);
+            AppResources.Culture = CrossMultilingual.Current.CurrentCultureInfo;
+
+            var newPage = new RTLExamplePage();
+            //newPage.FlowDirection = showRightToLeft ? FlowDirection.RightToLeft : FlowDirection.LeftToRight;
+            NavigationPage.SetBackButtonTitle(App.NavPage, AppResources.BackText);
+
+            Navigation.PushAsync(newPage);
+        }
+
+        void ShowEnglish(object sender, System.EventArgs e)
+        {
+            ShowLanguage("en", false);
+        }
+
+        void ShowArabic(object sender, System.EventArgs e)
+        {
+            ShowLanguage("ar", true);
+        }
+    }
+}
